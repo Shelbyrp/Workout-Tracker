a435631c843badf199c9c424e5401db490b7cf45
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-07-09 22:53:28 +0800

    Added post, get, put routes.

---
+++ routes/api.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const db = require("../models");
+
+router.post("/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+    res.send('Received a workout')
+});
+
+router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/workouts/:id", ({ params, body }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        {
+            $push: {
+                exercises: body,
+
+            },
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+module.exports = router;
